d0575558a02386287cb059b4739893d952f8dc8b
William Martinez <MARTWIL@chrobinson.com>
2019-01-01 18:16:44 -0600

    name change

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import './App.css';
 import { MuiThemeProvider } from '@material-ui/core';
 import theme from './theme';
-import NavBar from './drafter/components/NavBar';
+import NavBar from './common/components/NavBar';
 
 class App extends React.Component {
   public render() {
